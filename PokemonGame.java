11fbe281533b3c858b0728da06600f89ef03f304
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-02-06 22:31:34 +0700

    Update PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,9 +1,14 @@
-public class PokemonGame extends Pokemon {
-    public static void main(String[] args){
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+public class PokemonGame {
+    public static void main(String[] args) {
+        System.out.println("Start Game!");
         Pokemon A = new Pokemon();
-        A.setName("Pikachu");
-       
-        
-           
-    }
+        Random rand = new Random();   
+        for(int i=1 ; i <= 8 ;i++){
+            System.out.println(A.setType(rand.nextInt(i)));
+        }
+        A.start();
+    }      
 }
