ec1b46a1ae8406af5bdf8074902cae8323196f29
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-02-06 19:51:24 +0700

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Pokemon{
+    public static String name;
+    public String type;
+    public int level;
+    public int hp;
+    public int maxHP;
+    public int pp;
+    public int maxPP;
+    public int attackPower;
+
+    public class set{
+        Name = name ;
+        Type = type;
+        level = 100;
+        maxHP = 452;
+        hp = 0;
+        maxPP = 30;
+        pp = 0;
+        attackPower = 150;
+
+    }
+    public String setName(String name){
+        
+    }
+}
+
---
+++ PokemonGame.java
@@ -0,0 +1,21 @@
+public class PokemonGame extends Pokemon {
+    public static void main(String[] args){
+        java.lang.Object pikachu;{
+            Pokemon Object Object pokemonPika;
+            pikachu = new Object();
+            Pokemon Object Object pokemonEve;
+            java.lang.Object evee = new Object();
+            
+        }
+        public class playgame
+            if(pikachu.HP <= 0){
+                method.faint();
+                System.out.println(getClass() + "Win");
+            }
+            if(evee.HP <= 0){
+                method.faint();
+                System.out.println(getClass() + "Win");
+            }
+           
+    }
+}
