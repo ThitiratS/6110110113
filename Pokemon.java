42fa2a07e03cb82a0d05400a655cfa7038b80c82
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-02-06 21:34:04 +0700

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,27 +1,84 @@
-import java.io.*;
-public class Pokemon{
-    public static String name;
-    public String type;
-    public int level;
-    public int hp;
-    public int maxHP;
-    public int pp;
-    public int maxPP;
-    public int attackPower;
 
-    public class set{
-        Name = name ;
+
+public class Pokemon {
+    private String name;
+    private String type;
+    private int level;
+    private int exp;
+    private int levelExp;
+    private int maxLevel;
+    private int hp;
+    private int maxHP;
+    private int pp;
+    private int maxPP;
+    private int attackPower;
+
+    public Pokemon(String Name, String Type)
+    {
+        Name = name;
         Type = type;
-        level = 100;
-        maxHP = 452;
+        level = 0;
+        maxLevel = 100;
         hp = 0;
-        maxPP = 30;
+        maxHP = 452;
         pp = 0;
+        maxPP = 30;
         attackPower = 150;
 
     }
-    public String setName(String name){
-        
+    public void start()
+    {
+        System.out.println("Name :" + name);
+        System.out.println("Type :" + type);
+        System.out.println("Level :" + level);
+        System.out.println("HP :" + hp);
+        System.out.println("AttackPower :" + attackPower);
+
     }
+    public String setType(int getType) 
+    {
+        if(getType == 1)
+            return "Water";
+        else if(getType == 2)
+            return "Ground";
+        else if(getType == 3)
+            return "Fire";
+        else if(getType == 4)
+            return "Dark";
+        else if(getType == 5)
+            return "Ice";
+        else if(getType == 6)
+            return "Electric";
+        else if(getType == 7)
+            return "Flying";
+        else if(getType == 8)
+            return "Grass";
+        return type;
+       
+
+    }
+    
+    public void setHP() 
+    {
+       if(hp <= 0)
+       {
+            System.out.println("Faint!");
+       }
+
+    }
+    public void upLevel()
+    {
+        if(exp >= levelExp)
+        {
+            if(level <= maxLevel){
+                System.out.println("Level up!");
+                level++;
+            }
+            else if(level >= maxLevel){
+                System.out.println("Level max!");
+            }
+        }
+    }
+
 }
 
