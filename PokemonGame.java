3042a818c580966703996de57f8e638fe7618f63
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-02-06 19:55:06 +0700

    Update PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -2,13 +2,7 @@ public class PokemonGame extends Pokemon {
     public static void main(String[] args){
         Pokemon A = new Pokemon();
         A.setName("Pikachu");
-        A.setType("ICE");
-        A.setlevel(50);
-        A.sethp(5000);
-        A.setpp(100);
-        A.detail();
-        A.form(); 
-        System.out.println(" ");
+       
         
            
     }
