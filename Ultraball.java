84d87c29164e5102bbdf29e7521df81d80b66807
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:23:21 +0700

    Update Ultraball.java

--- Ultraball.java
+++ Ultraball.java
@@ -3,9 +3,5 @@ public class Ultraball extends PokemonBall {
     public Ultraball(String name){
         super(name,2);
     }
-    // public void randomball(Pokemon p){
-    //     int value = ((3*p.getMaxHp()) - (2*p.getHp()))* 2 * 100 / 10 * (3*p.getMaxHp());
-    //     setBall(value);
-    // }
 
 }
