81ed5e023ba89c7773fb5d162dbf20c24cf5f0f6
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:04:10 +0700

    Update Jigglypuff.java

--- Jigglypuff.java
+++ Jigglypuff.java
@@ -4,6 +4,7 @@ public class Jigglypuff extends Pokemon {
     public Jigglypuff(String name ,String type){
         super(name,type,340);
     }
+    
     public void attack(Pokemon enemy){
         System.out.print("Fight!!! " + name + " attack " + enemy.getName());
         enemy.damage(45);
