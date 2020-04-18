af11d0ddb1a5c749e1a3a53a3780f3d77e3da716
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 19:55:14 +0700

    Update Food.java

--- Food.java
+++ Food.java
@@ -7,37 +7,25 @@ public abstract class Food {
     public Food(String type,int num){
         this.type = type;
         this.number = num;
-        
-
     }
+    
     public String getType() {
         return type;
     }
+    
     public int getNum(){
         return number;
     }
+    
     public void setNum(int number){
         this.number = number;
     }
+    
     public int getValue(){
         return value;
     }
+    
     public abstract void useBerry(Pokemon pokemon,Food food);
     public abstract void reduceBerry(Food food);
-
-    // public boolean checkBerry(int value){
-    //     int curNum = number - value;
-    //     if(curNum == 0 ){
-    //         return false;
-    //     }
-    //     if(curNum > 0){
-    //         this.number = curNum;
-    //     }
-    //     else{
-    //         curNum = 0;
-    //     }
-    //     return true;
-    // }
-    
 
 }
