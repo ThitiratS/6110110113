2618f5dd6e7f9bab1795be85f3c0c87bcaa8d666
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:22:09 +0700

    Update ZenHeadbutt.java

--- ZenHeadbutt.java
+++ ZenHeadbutt.java
@@ -5,13 +5,13 @@ public class ZenHeadbutt implements Posture {
     private String name;
 
     public ZenHeadbutt(String name){
+        
         this.name = name;
         this.cp = 12;
     }
 
     public void setName(String name) {
         this.name = name;
-
     }
 
     public String getName() {
@@ -28,7 +28,9 @@ public class ZenHeadbutt implements Posture {
         
         return cp;
     }
-    public String tosString(){
+    
+    public String toString(){
+        
         return name + " " + cp;
     }
 }
