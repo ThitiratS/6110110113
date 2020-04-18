b95f533522deee4e488215a312020df6d5c4a5ca
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 19:33:40 +0700

    Add files via upload

---
+++ BallRandomizer.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class BallRandomizer {
+    public static ArrayList<PokemonBall> getBall(int num){
+        
+        ArrayList<PokemonBall> pokemonballs = new ArrayList<PokemonBall>();
+        if(num < 1){
+            return pokemonballs;
+        }
+        
+
+        for(int i = 0 ; i < num; ++i){
+            int type = (int)(Math.random()*5);
+            if (type == 0){
+                pokemonballs.add(new Pokeball("Pokeball"));
+            } 
+            else if(type == 1){
+                pokemonballs.add(new Quickball("Quickball"));
+            } 
+            else if(type == 2){
+                pokemonballs.add(new Timerball("Timerball"));
+            }  
+            else if(type == 3){
+                pokemonballs.add(new Ultraball("Ultraball"));
+            }  
+            else if(type == 4){
+                pokemonballs.add(new Masterball("Masterball"));
+            }  
+            
+        }
+        return pokemonballs;
+    }
+
+}
