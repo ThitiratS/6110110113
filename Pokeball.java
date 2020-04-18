9fb8abfad12efc3618809524eb6980cb3b4bcb91
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:35:01 +0700

    Update Pokeball.java

--- Pokeball.java
+++ Pokeball.java
@@ -3,8 +3,5 @@ public class Pokeball extends PokemonBall{
     public Pokeball(String name){
         super(name,1);
     }
-    // public void randomball(Pokemon p){
-    //     int value = ((3*p.getMaxHp()) - (2*p.getHp()))* 1 * 100 / 10 * (3*p.getMaxHp());
-    //     setBall(value);
-    // }
+   
 }
