1f8947876ad5b143a261319cdf159f0d9aff8cd6
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 19:50:07 +0700

    Update Eatery.java

--- Eatery.java
+++ Eatery.java
@@ -23,40 +23,39 @@ public class Eatery extends JFrame {
         jLabel1.setText("Pokemon Number");
         getContentPane().add(jLabel1);
         jLabel1.setBounds(160, 60, 140, 18);
-
+        
         txtName.setName("txtName"); // NOI18N
         txtName.setSize(180, 150);
         getContentPane().add(txtName);
         txtName.setBounds(80, 100, 260, 20);
-      	btnClick.setText("Click");
+        btnClick.setText("Click");
 
         btnClick.addActionListener(new ActionListener(){
-			//anonymous class
-		public void actionPerformed(ActionEvent e) { 
-
-                	try {
-                    		no = txtName.getText();
-                    		int result = Integer.parseInt(no);
-                    		//System.out.println(result);
-                   		for(int i = 0; i < bag.size() ; i++){
-                        	//System.out.println(wildPokemon );
-                        		if(result == i){
-                           			Pokemon myPokemon = bag.get(i);
-                            			Feed f = new Feed(myPokemon, r, b, p);
-                           			f.use(myPokemon, r, b, p);
+	    //anonymous class
+	    public void actionPerformed(ActionEvent e) { 
                 
-                           			// System.out.println(bag);
-                       			}
-                    		}
+                try {
+                    no = txtName.getText();
+                    int result = Integer.parseInt(no);
+                    //System.out.println(result);
+                    for(int i = 0; i < bag.size() ; i++){
+                        //System.out.println(wildPokemon );
+                        if(result == i){
+                            Pokemon myPokemon = bag.get(i);
+                            Feed f = new Feed(myPokemon, r, b, p);
+                            f.use(myPokemon, r, b, p);
+                            System.out.println(bag);
+                        }
+                    }
                     
-               		} catch (NumberFormatException evt) {
+                } catch (NumberFormatException evt) {
                    
-                    		JOptionPane.showMessageDialog(null, "ErrorMsg","Please Enter Number", JOptionPane.ERROR_MESSAGE);
-                	}
-                	//btnClickActionPerformed(e);
-                	setVisible(false);
+                    JOptionPane.showMessageDialog(null, "ErrorMsg","Please Enter Number", JOptionPane.ERROR_MESSAGE);
+                }
+                //btnClickActionPerformed(e);
+                setVisible(false);
                 
-		}
+	    }
 	});
 
         getContentPane().add(btnClick);
