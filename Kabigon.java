e50ce0746544f7b1f3f981f2d95f1b1d0d4bef11
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:04:55 +0700

    Update Kabigon.java

--- Kabigon.java
+++ Kabigon.java
@@ -1,15 +1,18 @@
 
 public class Kabigon extends Pokemon{
+	
     public Kabigon(String name ,String type){
         super(name,type,1000);
     }
+	
     public Kabigon(String name, String type, int maxHP) {
         super(name, type, maxHP);
-	}
-	public void attack(Pokemon enemy){
+    }
+	
+    public void attack(Pokemon enemy){
         System.out.print("Fight!!! " + name + " attack " + enemy.getName());
-        enemy.usePosture(enemy);
-        
+        enemy.usePosture(enemy);       
         System.out.println(" " + enemy.getHp());
     }
+	
 }
