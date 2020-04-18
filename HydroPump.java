1329b083bfbc918039aff5c4fb259e43b0f817aa
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 20:03:13 +0700

    Update HydroPump.java

--- HydroPump.java
+++ HydroPump.java
@@ -5,13 +5,14 @@ public class HydroPump  implements Posture {
     private String name;
 
     public HydroPump(String name){
+        
         this.name = name;
         this.cp = 40;
     }
 
     public void setName(String name) {
+        
         this.name = name;
-
     }
 
     public String getName() {
@@ -28,7 +29,9 @@ public class HydroPump  implements Posture {
         
         return cp;
     }
+    
     public String tosString(){
+        
         return name + " " + cp;
     }
 }
