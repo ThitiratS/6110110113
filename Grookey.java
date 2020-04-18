cd94f90abe67ea56f7a35fa9098dafe7d7fe2756
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-18 19:55:33 +0700

    Update Grookey.java

--- Grookey.java
+++ Grookey.java
@@ -4,11 +4,12 @@ public class Grookey extends Pokemon {
     public Grookey(String name ,String type){
         super(name,type,50);
     }
+    
     public void attack(Pokemon enemy){
         System.out.print("Fight!!! " + name + " attack " + enemy.getName());
         enemy.damage(65);
         System.out.println(" " +enemy.getHp());
+            
     }
-
 
 }
