b7659e78863922c08bd9aa9cbd260858c3829843
ThitiratS <60661456+ThitiratS@users.noreply.github.com>
2020-04-21 16:50:00 +0700

    Update Hatching2.java

--- Hatching2.java
+++ Hatching2.java
@@ -9,14 +9,14 @@ import java.net.URL;
 import java.util.*;
 
 public class Hatching2 extends JFrame {
-    
+  
     private static final long serialVersionUID = 1L;
 
-    public Hatching2(Pokemon male,Pokemon female,ArrayList<Pokemon> partners) {
+    public Hatching2(Pokemon male, Pokemon female, ArrayList<Pokemon> partners) {
 
         super("Eggs Hatching ");
         Container c = getContentPane();
-        
+        ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
         Font myFont = null;
         
         
@@ -46,16 +46,105 @@ public class Hatching2 extends JFrame {
         p1.setBorder(BorderFactory.createEmptyBorder(14, 14, 14, 14));
         p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
 
+        if(male.getType() == "Electric" || female.getType() == "Electric" ){
+            for(int i =0 ; i < 1 ;++i){
+                int type = (int)(Math.random()*3);
+                if(type == 0){
+                    pokemons.add(new Electabuzz("Electabuzz","Electric"));
+                    
+                }
+                if(type == 1){
+                    pokemons.add(new Voltorb("Voltorb","Electric"));
+                   
+                }
+                if(type == 2){
+                    pokemons.add(new Pichu("Pichu","Electric"));
+                   
+                }
+            }
+        }
+        if(male.getType() == "Water" ||  female.getType() == "Water"){
+            for(int i =0 ; i < 1 ;++i){
+                int type = (int)(Math.random()*3);
+                if(type == 0){
+                    pokemons.add(new Sobble("Sobble","Water"));
+                   
+                }
+                if(type == 1){
+                    pokemons.add(new Drednaw("Drednaw","Water"));
+                   
+                }
+                if(type == 2){
+                    pokemons.add(new Krabby("Krabby","Water"));
+                   
+                }
+            }
+        }
+        if(male.getType() == "Grass" || female.getType() == "Grass"){
+            for(int i =0 ; i < 1 ;++i){
+                int type = (int)(Math.random()*3);
+                if(type == 0){
+                    pokemons.add(new Shiftry("Shiftry","Grass"));
+                   
+                }
+                if(type == 1){
+                    pokemons.add(new Lombre("Lombre","Grass"));
+                  
+                }
+                if(type == 2){
+                    pokemons.add(new Grookey("Grookey","Grass"));
+                    
+                }
+            }
+        }
+        if(male.getType() == "Normal" || female.getType() == "Normal" ){
+            for(int i =0 ; i < 1 ;++i){
+                int type = (int)(Math.random()*3);
+                if(type == 0){
+                    pokemons.add(new Kangaskhan("Kangaskhan","Normal"));
+                   
+                }
+                if(type == 1){
+                    pokemons.add(new Eevee("Eevee","Normal"));
+                   
+                }
+                if(type == 2){
+                    pokemons.add(new Jigglypuff("Jigglypuff","Normal"));
+                    
+                }
+            }
+        }
+        int no = 0;
+        
+        if(pokemons.size() > 1){
+           
+            for(int i = 0 ;i < 1 ; i++){
+                int t = (int)(Math.random()*2);
+                if(t == 0){
+                   
+                   no = 0;
+
+                }
+                else if(t == 1){
+
+                    no = 1;
+                }
+              
+            }
+        }
+
+        Pokemon p = pokemons.get(no); 
+
         JButton b1 = new JButton("HATCH");
         b1.add(Box.createRigidArea(new Dimension(245, 25)));
         b1.setFont(b1.getFont().deriveFont(Font.ITALIC,18.0f));
         b1.addActionListener(new ActionListener(){
 	    //anonymous class
 	    public void actionPerformed(ActionEvent e) {
-                new Hatching3(male, female,partners);
+                new Hatching3(p,partners);
                 setVisible(false);
             }
-	});
+		});
 
         JButton b2 = new JButton("CANCLE");
         b2.add(Box.createRigidArea(new Dimension(245, 25)));
@@ -66,6 +155,7 @@ public class Hatching2 extends JFrame {
                 setVisible(false);
             }
         });
+
         p1.setBackground(Color.BLACK);
         p1.add(b1);
         p1.add(b2);
@@ -81,7 +171,6 @@ public class Hatching2 extends JFrame {
         c.add(pk1, BorderLayout.NORTH);
         c.add(p2, BorderLayout.CENTER);
         c.add(p1, BorderLayout.PAGE_END);
-	    
         setSize(600, 620);
         //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
